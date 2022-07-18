79553bbfd2b5fa5322658bdb8ca7a9543a52affd
shuimuliang <shuimuliang@gmail.com>
2022-07-18 17:14:34 +0800

    test asm

--- src/train/fixme_replace_all_dots.rs
+++ src/train/fixme_replace_all_dots.rs
@@ -1,4 +1,18 @@
+use std::arch::asm;
+
 fn replace_dots(s: &str) -> String {
+    let mut x: u64 = 4;
+    unsafe {
+        asm!(
+        "mov {tmp}, {x}",
+        "shl {tmp}, 1",
+        "shl {x}, 2",
+        "add {x}, {tmp}",
+        x = inout(reg) x,
+        tmp = out(reg) _,
+        );
+    }
+    assert_eq!(x, 4 * 6);
     s.replace(".", "-").to_string()
 }
 
