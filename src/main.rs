663e9937ba738bbdc3047b0b276ff4b649ae34e9
cairo <cairo@cairo-m1.local>
2022-07-18 13:10:40 +0800

    readme

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,41 @@
+// http://ref.x86asm.net/coder32.html
+// enum OpCode {
+//     MOV,
+//     INC,
+//     DEC,
+//     ADD,
+//     SUB,
+//     MUL,
+//     DIV,
+//     JMP,
+//     CMP,
+//     JNE,
+//     JE,
+//     JGE,
+//     JG,
+//     JLE,
+//     JL,
+//     CALL,
+//     RET,
+//     END,
+//     COMMENT
+// // label:
+// }
+
+// memory.store(self.sp--, value);
+// var value = memory.load(++self.sp);
+
+use itertools::Itertools;
+
 fn main() {
+
+    let perms = (5..8).permutations(2);
+    itertools::assert_equal(perms, vec![
+        vec![5, 6],
+        vec![5, 7],
+        vec![6, 5],
+        vec![6, 7],
+        vec![7, 5],
+        vec![7, 6],
+    ]);
 }
